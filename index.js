74d8c61d48406ce34e4cec8e7d99e8869147270c
qudroturkey <reesemb1@gmail.com>
2020-03-16 13:44:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+/* eslint-disable no-console */
+function superbowlWin(record) {
+  const result = record.find((superbowl) => superbowl.result === 'W');
+  if (result === undefined) return undefined;
+  return (result.year);
 }
