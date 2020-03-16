59003f3237770309b834b1fdae6ded5cfeecb564
Jp1200 <jprkr96@gmail.com>
2020-03-16 14:39:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const testVar = {}
 
-function testFunc() {
-  return "hi"
+function superbowlWin(inputArray) {
+  let win = inputArray.find( e =>
+    e.result === 'W'
+
+
+
+  )
+  if (win == undefined){
+    return undefined
+  }
+  else {
+    return win.year
+  }
 }
