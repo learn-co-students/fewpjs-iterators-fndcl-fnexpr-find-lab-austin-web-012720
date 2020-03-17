f7461229a0ea38d88fdce7908612e31f260e403a
github username <github email address>
2020-03-17 14:16:00 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,25 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+superbowlWin = (record) => {
+  let result = record.find( object => object.result === 'W')
+  if (result) {
+    return result.year  
+  } else {
+    return undefined
+  }
+}
+
+//Receives 1 argument, an Array of JavaScript Objects
+//Each object has two properties: year and result
+//Use find() to test each Object to see if the result is "W" — a win!
+//superbowlWin() should return the year when the win occurred 
+//(if it occurred at all!). If no win is found, it should return, sadly, undefined
+
+// const record = [
+//    {year: "2018", result: "L"},
+//    {year: "2017", result: "W"},
+//    {year: "2016", result: "N/A"}
+// //   //...
+// ]
