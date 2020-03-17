11af42fad3f39e027e37819cb687f59d5da72bb8
mattjagiello <mjagiello@gmail.com>
2020-03-17 14:59:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const testVar = {}
+const testVar = {};
 
 function testFunc() {
-  return "hi"
+  return 'hi';
 }
+
+// const record = [
+//   {year: "2018", result: "L"},
+//   {year: "2017", result: "W"},
+//   {year: "2016", result: "N/A"}
+//   //...
+// ]
+
+function superbowlWin(record) {
+  const win = record.find((record) => record.result === 'W');
+  if (win) return win.year;
+  return undefined;
+}
+
+superbowlWin();
