94a93806e3ad826133be83ff975af1b6e33e4025
jbshipman <jbshipman@icloud.com>
2020-03-22 09:15:46 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
+// function superbowlWin(arr) {
+//   return arr === 'W';
+// };
+
+function superbowlWin(arg) {
+  let result = arg.find( arg => arg.result === "W" );
+  return result ? result.year : undefined;
+};
